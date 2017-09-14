22f18f4420bb6cd2b3eaab23b517a8bec011b914
Devansh Jain <j.devansh29@gmail.com>
2017-09-13 17:29:01 -0700

    add textfile containing words

--- complete_hangman.py
+++ complete_hangman.py
@@ -16,35 +16,35 @@ def return_word(input_list):
 
 def hangman_logic(word,difficulty):
 	current_word = ["*" for n in range(len(word))]
-	print return_word(current_word)
+	print (return_word(current_word))
 	guessed_letters = ""
 	counter = 0
 	while True:
 		user_guess = raw_input("What is your guess?: \n")
 		if len(user_guess) != 1:
-			print "please input a single letter"
+			print ("please input a single letter")
 		elif user_guess in guessed_letters:
-			print "Wait, you've already guessed that."
+			print ("Wait, you've already guessed that.")
 		else:
 			guessed_letters += user_guess
 			if user_guess not in word:
 				counter += 1
 				if counter == difficulty:
-					print "Sorry you lose, the word was %s" %word
+					print("Sorry you lose, the word was %s" %word)
 					return False
-				print "Sorry, thats not in there, and you have %s guesses left." % (difficulty - counter)
+				print ("Sorry, thats not in there, and you have %s guesses left." % (difficulty - counter))
 			else:
-				print "Yes, the word contains a/an %s!" %user_guess
+				print ("Yes, the word contains a/an %s!" %user_guess)
 				letter_indices = [m.start() for m in re.finditer(user_guess, word)]
 				for x in letter_indices:
 					current_word[x] = user_guess
 		if "*" not in return_word(current_word):
-			print "You got it!, the word is", word
+			print ("You got it!, the word is", word)
 			return True
-		print "the word so far is: ",return_word(current_word)
+		print ("the word so far is: ",return_word(current_word))
 		#print "letters not in the word: ",guessed_letters
-	print "the word is: ",return_word(current_word)	
-	print "You took %s guesses." %counter
+	print ("the word is: ",return_word(current_word))	
+	print ("You took %s guesses." %counter)
 
 def play_hangman():
 	won = 0
@@ -67,9 +67,9 @@ def play_hangman():
 			else:
 				lost += 1
 		else:
-			print "thanks for playing, you won %s games and lost %d games." % (won,lost)
+			print("thanks for playing, you won %s games and lost %d games." % (won,lost))
 			break
-		print won,lost
+		print ("you've won:", won, "and lost", lost)
 	
 play_hangman()
 
